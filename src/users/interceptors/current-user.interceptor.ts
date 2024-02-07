0154593a3556c842c6e3e9b726899274252d366f
Irfan <irfan.ik227@gmail.com>
2024-02-08 03:49:40 +0500

    user interceptor update

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -18,7 +18,7 @@ export class CurrentUserInterceptor implements NestInterceptor {
     const request = context.switchToHttp().getRequest();
     const { userId } = request.session || {};
     if (userId) {
-      const user = await this.usersService.findOne(request.session.userId);
+      const user = await this.usersService.findOne(userId);
       request.currentUser = user;
     }
     return next.handle();
